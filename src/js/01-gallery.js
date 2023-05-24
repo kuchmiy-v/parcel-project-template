2acc3f3f30786b5d6164a9c1ac76875d32af0e03
kuchmiy-v <wrdvrk@gmail.com>
2023-05-24 19:09:25 +0300

    task1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (item) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join("");
+galleryList.innerHTML = galleryMarkup;
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionsDelay: 250,
+  });
