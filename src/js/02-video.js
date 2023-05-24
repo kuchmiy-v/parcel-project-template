3130a93b98de761db7c525309998ef2b689bbb8f
kuchmiy-v <wrdvrk@gmail.com>
2023-05-24 19:46:39 +0300

    probna zdacha

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 import "../css/common.css";
 import Player from "@vimeo/player";
-import { throttle } from "lodash";
+import throttle from 'lodash.throttle';
 
 const TIME_KEY = "videoplayer-current-time";
 const iframe = document.querySelector("iframe");
